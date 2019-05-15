f398b80cdb597dcac97c8ddd9b8855c4ac454db2
David Acosta <davidacosta25081@yahoo.com>
2019-05-15 20:13:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,8 +13,11 @@ class App < Sinatra::Base
     "#{@square.to_s}"
   end
 
-
-
+  get "/say/:number/:phrase" do 
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase * @number}"
+  end
 
 
 
